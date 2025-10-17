9cfb35b8fc7dbe8c9636c682889d8a8c7f4d4acf
Anubisworkingexperience <79977978+Anubisworkingexperience@users.noreply.github.com>
2025-10-17 22:10:19 +0300

    Add sql script

---
+++ schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE nomenclature (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(500) NOT NULL,
+  quantity INT NOT NULL,
+  price DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(500) NOT NULL,
+  parent_id INT REFERENCES categories(id)
+);
+
+CREATE TABLE clients (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(500) NOT NULL,
+  address VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  client_id INT REFERENCES clients(id),
+  order_date DATE
+);
+
+CREATE TABLE order_items (
+  id SERIAL PRIMARY KEY,
+  order_id INT REFERENCES orders(id),
+  nomenclature_id INT REFERENCES nomenclature(id),
+  quantity INT NOT NULL
+  price DECIMAL(10, 2) NOT NULL
+);
+
